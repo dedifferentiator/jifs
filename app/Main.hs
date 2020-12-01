664bfc512882efb426e251e75762431b51cb7816
413x <dedifferentiator@protonmail.ch>
2020-12-01 12:18:55 +0200

    add: haddock comments support; minor improvements

--- app/Main.hs
+++ app/Main.hs
@@ -79,14 +79,14 @@ nFSOps = defaultFuseOperations
 foreign import ccall "posix_fallocate"
   c_posix_fallocate :: CInt -> COff -> COff -> IO CInt
 
--- implements (@posix_fallocate(3)@)
+-- | implements (@posix_fallocate(3)@)
 nFallocate :: Handle -> CInt -> FileOffset -> FileOffset -> IO Errno
 nFallocate _ mode _ _ | mode /= 0 = pure eOPNOTSUPP
 nFallocate h _ offset len = do
   (Fd fd) <- handleToFd h
   Errno <$> c_posix_fallocate fd offset len
 
--- implements @setxattr(2)@
+-- | implements @setxattr(2)@
 nSetxattr :: FilePath -> String -> B.ByteString -> SetxattrFlag -> IO Errno
 nSetxattr path name value flag =
   let f = case flag of
@@ -95,34 +95,34 @@ nSetxattr path name value flag =
             SetxattrReplace -> lReplaceXAttr
   in tryErrno_ $ f path name value
 
--- implements @getxattr(2)@.
+-- | implements @getxattr(2)@.
 nGetxattr :: FilePath -> String -> IO (Either Errno B.ByteString)
 nGetxattr path name = tryErrno $ lGetXAttr path name
 
--- implements (POSIX @lstat(2)@)
+-- | implements (POSIX @lstat(2)@)
 nGetAttr :: FilePath -> Maybe Handle -> IO (Either Errno FileStat)
 nGetAttr path _ = tryErrno . getFileStat $ path
 
--- implements (POSIX @mkdir(2)@)
+-- | implements (POSIX @mkdir(2)@)
 nMkdir :: FilePath -> FileMode -> IO Errno
 nMkdir path mode = tryErrno_ $ createDirectory path mode
 
--- implements (POSIX @mknod(2)@)
+-- | implements (POSIX @mknod(2)@)
 nMknod :: FilePath -> FileMode -> DeviceID -> IO Errno
 nMknod path mode rdev = tryErrno_ $ case fileModeToEntryType mode of
   RegularFile -> bracket
                  (openFd path WriteOnly (Just mode) (defaultFileFlags{exclusive=True}))
                  closeFd
-                 (\_ -> pure ())
+                 (const $ pure ())
   Directory -> createDirectory path mode
   NamedPipe -> F.createNamedPipe path mode
   _ -> F.createDevice path mode rdev
 
--- implements (POSIX @rmdir(2)@)
+-- | implements (POSIX @rmdir(2)@)
 nRmdir :: FilePath -> IO Errno
 nRmdir = tryErrno_ . removeDirectory
 
--- implements @readdir(3)@
+-- | implements @readdir(3)@
 nReadDirectory :: FilePath -> IO (Either Errno [(FilePath, Maybe FileStat)])
 nReadDirectory path = tryErrno
   $ bracket (openDirStream path) closeDirStream
@@ -133,13 +133,13 @@ nReadDirectory path = tryErrno
       then pure acc
       else loop $ (entry, Nothing) : acc
 
--- merges OpenMode and OpenFileFlags to IOMode
+-- | merges OpenMode and OpenFileFlags to IOMode
 toIOMode :: OpenMode -> OpenFileFlags -> IOMode
 toIOMode ReadOnly _ = ReadMode
 toIOMode WriteOnly (OpenFileFlags a _ _ _ _) = if a then AppendMode else WriteMode
 toIOMode ReadWrite _ = ReadWriteMode
 
--- outputs to stdout mode and file control flags
+-- | outputs to stdout mode and file control flags
 nDebugPrintMode :: FilePath
                 -> OpenMode
                 -> OpenFileFlags
@@ -159,22 +159,22 @@ nDebugPrintMode path mode flags hFD = do
           <> "\n"
   hFD
 
--- implements (POSIX @open(2)@)
+-- | implements (POSIX @open(2)@)
 nOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno Handle)
 nOpen path mode flags = nDebugPrintMode path mode flags $
   Right <$> openFile path (toIOMode mode flags)
 
--- implements  Unix98 @pread(2)@
+-- | implements  Unix98 @pread(2)@
 nRead :: FilePath -> Handle -> ByteCount -> FileOffset -> IO (Either Errno B.ByteString)
 nRead _ h count offset = do
   hSeek h AbsoluteSeek (toInteger offset)
   Right <$> B.hGet h (fromInteger $ toInteger count)
 
--- closes handle, calling on already closed handle has no effect
+-- | closes handle, calling on already closed handle has no effect
 nRelease :: FilePath -> Handle -> IO ()
 nRelease _ = hClose
 
--- forces all buffered for the output items to be sent to the OS
+-- | forces all buffered for the output items to be sent to the OS
 nFlush :: FilePath -> Handle -> IO Errno
 nFlush _ h = catch
   (hFlush h >>= (\_ -> return eOK))
@@ -183,28 +183,29 @@ nFlush _ h = catch
       hPutStr stderr $ "Cannot flush file: " <> err
       return eACCES)
 
--- implements Unix98 @pwrite(2)@
+-- | implements Unix98 @pwrite(2)@
 nWrite :: FilePath -> Handle -> B.ByteString -> FileOffset -> IO (Either Errno CInt)
 nWrite _ h content offset = do
   hSeek h AbsoluteSeek (toInteger offset)
   B.hPut h content
   return $ Right . fromInteger . toInteger $ B.length content
 
+-- | implements (POSIX @readlink(2)@)
 nReadLink :: FilePath -> IO (Either Errno FilePath)
 nReadLink = tryErrno . F.readSymbolicLink
 
---implements (POSIX @link(2)@).
+-- | implements (POSIX @link(2)@)
 nCreateLink :: FilePath -> FilePath -> IO Errno
 nCreateLink src dst = tryErrno_ $ F.createLink src dst
 
--- implements (POSIX @unlink(2)@)
+-- | implements (POSIX @unlink(2)@)
 nSymLink :: FilePath -> FilePath -> IO Errno
 nSymLink src dst = tryErrno_ $ F.createSymbolicLink src dst
 
--- implements (POSIX @unlink(2)@)
+-- | implements (POSIX @unlink(2)@)
 nUnlink :: FilePath -> IO Errno
 nUnlink = tryErrno_ . F.removeLink
 
--- implements @statfs(2)@.
+-- | implements @statfs(2)@
 nStatFS :: FilePath -> IO (Either Errno FileSystemStats)
 nStatFS = tryErrno . getFileSystemStats
